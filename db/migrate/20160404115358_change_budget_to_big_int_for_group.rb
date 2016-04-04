8240592412c0d83535515e6ce931891518748be9
Peter Kappus <peterk@peterkappus.com>
2016-04-04 13:48:06 +0100

    remove limit on budget so bigint works.

--- db/migrate/20160404115358_change_budget_to_big_int_for_group.rb
+++ db/migrate/20160404115358_change_budget_to_big_int_for_group.rb
@@ -1,5 +1,5 @@
 class ChangeBudgetToBigIntForGroup < ActiveRecord::Migration
   def change
-    change_column :groups, :budget_pennies,  :bigint
+    change_column :groups, :budget_pennies,  :bigint, :limit => nil
   end
 end
